6da39fc1b4ab8322b8d789c65c9850ffaccc43cc
leok90 <leok90@gmail.com>
2018-02-13 20:57:59 +0000

    Sends hard-coded message to hard-coded chat ID when /done is hit

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask
 from flask import request
-from bot import run
+from bot import run, send_message
 
 app = Flask(__name__)
 
@@ -8,14 +9,18 @@ app = Flask(__name__)
 def receive():
     try:
         run(request.json)
-        return ""
+        return "Hello"
     except Exception as e:
         print(e)
-        return ""
+        return "error of some description"
 
 @app.route("/done", methods=["GET", "POST"])
 def done():
     """
-    Send a message to me telling me that the thing is done"
+    Send a message to me telling me that the thing is done
     """
-    return "the thing is done"
+    try:
+        send_message("The thing is done", os.environ['TELEGRAM_CHAT_ID'])
+    except:
+        return "I tried to send the message to you but it didn't work"
+    return "The thing happened"
